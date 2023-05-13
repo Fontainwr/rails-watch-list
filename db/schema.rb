e032238bdf4f51c33ca6529186e4ddea8b8a1fb6
fontain richardson <fontainwr@gmail.com>
2023-05-13 11:29:41 +0900

    new

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_11_112007) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_12_005009) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,8 +43,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_11_112007) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.integer "rating"
-    t.text "comment"
+    t.float "rating"
+    t.string "committed!"
     t.bigint "list_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
